05f0fddd90056e9299749b6dbf837f5ee67e877d
Marcia Silva <marciamaria_brasil@hotmail.com>
2026-04-24 01:49:51 -0300

    Ajusta intercept com add, update, remove e uso de cookie conforme atividade

--- cypress/e2e/cypress/carrinho-intercept.cy.js
+++ cypress/e2e/cypress/carrinho-intercept.cy.js
@@ -1,13 +1,56 @@
 describe('Carrinho de compras com intercept', () => {
 
-  it('Deve validar intercept funcionando', () => {
-
-    cy.intercept('GET', '**/products*').as('getProducts')
-
+  beforeEach(() => {
+    cy.setCookie('ebacStoreVersion', 'v2')
     cy.visit('https://automationexercise.com/products')
+  })
 
-    cy.wait('@getProducts').then((interception) => {
+  it('Deve simular e validar adicionar, atualizar e remover item do carrinho', () => {
+
+    cy.intercept('POST', '**/cart/add', {
+      statusCode: 200,
+      body: {
+        success: true,
+        message: 'Produto adicionado ao carrinho'
+      }
+    }).as('adicionarItem')
+
+    cy.intercept('PUT', '**/cart/update', {
+      statusCode: 200,
+      body: {
+        success: true,
+        quantity: 2,
+        message: 'Quantidade atualizada'
+      }
+    }).as('atualizarItem')
+
+    cy.intercept('DELETE', '**/cart/remove', {
+      statusCode: 200,
+      body: {
+        success: true,
+        message: 'Produto removido do carrinho'
+      }
+    }).as('removerItem')
+
+    cy.window().then((win) => {
+      win.fetch('/cart/add', { method: 'POST' })
+      win.fetch('/cart/update', { method: 'PUT' })
+      win.fetch('/cart/remove', { method: 'DELETE' })
+    })
+
+    cy.wait('@adicionarItem').then((interception) => {
       expect(interception.response.statusCode).to.eq(200)
+      expect(interception.response.body.success).to.eq(true)
+    })
+
+    cy.wait('@atualizarItem').then((interception) => {
+      expect(interception.response.statusCode).to.eq(200)
+      expect(interception.response.body.quantity).to.eq(2)
+    })
+
+    cy.wait('@removerItem').then((interception) => {
+      expect(interception.response.statusCode).to.eq(200)
+      expect(interception.response.body.success).to.eq(true)
     })
 
   })
